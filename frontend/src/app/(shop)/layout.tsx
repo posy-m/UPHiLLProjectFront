55e6bbc8f67e50689a07b32e2e632a2fef0a8352
Jaka Park <sangcheol@Jaka-ui-MacBookPro.local>
2024-10-13 16:45:38 +0900

    test

--- frontend/src/app/(shop)/layout.tsx
+++ frontend/src/app/(shop)/layout.tsx
@@ -7,7 +7,6 @@ const Layout = ({children}:{children:React.ReactNode}) => {
   return (
     <QueryClientProvider client={queryClient}>
       <main>{children}</main>
-      
     </QueryClientProvider>
   )
 }
