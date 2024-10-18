630d4b1b389cf632f3e4bdd0d7ff8e90fd07b200
posy-m <minji.posy@gmail.com>
2024-10-18 20:01:03 +0900

    상품 관리자 css 수정

--- frontend/src/app/(shop)/shop/product/comoponents/Admin.tsx
+++ frontend/src/app/(shop)/shop/product/comoponents/Admin.tsx
@@ -1,5 +1,5 @@
 'use client';
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import Link from 'next/link';
 import AddBtn from './AddBtn';
 import Popup from './Popup';
@@ -10,8 +10,9 @@ import Product from './Product';
 import Modify from '../../avatar/comoponents/Modify';
 import Header from '@/app/_components/header/header';
 import Footerbar from '@/app/_components/footerbar/footerbar';
-import {useInfiniteQuery} from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { UseUserScroll } from '../../hooks/useScroll';
+import { ProductScroll } from '../../hooks/productScroll';
 
 const Admin = () => {
   const [isPopup, setIsPopup] = useState<boolean>(false);
@@ -20,7 +21,7 @@ const Admin = () => {
   const [dataLength, setDataLength] = useState(0);
 
   const dataCountAxios = async () => {
-    const {data} = await customAxios.get('/shop/product/count');
+    const { data } = await customAxios.get('/shop/product/count');
     setDataLength(data);
   };
 
@@ -42,7 +43,7 @@ const Admin = () => {
     queryKey: ['infinitescroll'],
     queryFn: getProductPages,
     initialPageParam: 1,
-    getNextPageParam(lastPage, allPages){
+    getNextPageParam(lastPage, allPages) {
       // 페이지가 남아있으면 더 추가해주는 로직
       return allPages.length < Math.ceil(dataLength / 10) ? allPages.length + 1 : undefined;
     }
@@ -56,26 +57,26 @@ const Admin = () => {
       <div className={styled.avatar_wrap}>
         <div className={styled.avatar_content}>
           <ul className={styled.product_ul}>
-            <li ><Link href="http://localhost:3000/shop/avatar">아바타</Link></li>
-            <li style={{borderBottom:"3px solid rgb(112, 61, 22)", color: "rgb(112, 61, 22)", boxSizing: "border-box"}}>상품</li>
+            <li ><Link href="http://127.0.0.1:3000/shop/avatar">아바타</Link></li>
+            <li>상품</li>
           </ul>
-          <UseUserScroll
-            fetchNextPage={fetchNextPage} 
-            hasNextPage={hasNextPage} 
-            isFetchingNextPage={isFetchingNextPage} 
+          <ProductScroll
+            fetchNextPage={fetchNextPage}
+            hasNextPage={hasNextPage}
+            isFetchingNextPage={isFetchingNextPage}
             data={data}
-            >
-            {data?.pages.map((page) => page.map((e:any) =>
+          >
+            {data?.pages.map((page) => page.map((e: any) =>
               <li className={styled.avatar_list} key={e.id}>
-                <Product productId={e.id} name={e.name} refetch={refetch} setProductId={setProductId} setModifyPopup={setModifyPopup} modifyPopup={modifyPopup} price={e.price} image={e.image}/>
+                <Product productId={e.id} name={e.name} refetch={refetch} setProductId={setProductId} setModifyPopup={setModifyPopup} modifyPopup={modifyPopup} price={e.price} image={e.image} />
               </li>))
-            } 
-          </UseUserScroll>
+            }
+          </ProductScroll>
         </div>
-        <AddBtn isPopup={isPopup} setIsPopup={setIsPopup} modifyPopup={modifyPopup}/>
-        { isPopup ? <Popup refetch={refetch} isPopup={isPopup} setIsPopup={setIsPopup}/> : '' }
-        { modifyPopup ? <Modify refetch={refetch} productId={productId} setModifyPopup={setModifyPopup} modifyPopup={modifyPopup}/> : '' }
-        { modifyPopup ? <Modify refetch={refetch} productId={productId} setModifyPopup={setModifyPopup} modifyPopup={modifyPopup}/> : '' }
+        <AddBtn isPopup={isPopup} setIsPopup={setIsPopup} modifyPopup={modifyPopup} />
+        {isPopup ? <Popup refetch={refetch} isPopup={isPopup} setIsPopup={setIsPopup} /> : ''}
+        {modifyPopup ? <Modify refetch={refetch} productId={productId} setModifyPopup={setModifyPopup} modifyPopup={modifyPopup} /> : ''}
+        {modifyPopup ? <Modify refetch={refetch} productId={productId} setModifyPopup={setModifyPopup} modifyPopup={modifyPopup} /> : ''}
       </div>
       <Footerbar />
     </>
